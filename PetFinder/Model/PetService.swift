1fb4c5d1252d5a7448f5e590a35b085ef00b2bfb
lefreepatate <c_garcia_muskat@hotmail.com>
2019-03-25 10:48:06 +0100

    Hierarchy created

---
+++ PetFinder/Model/PetService.swift
@@ -0,0 +1,12 @@
+//
+//  PetService.swift
+//  PetFinder
+//
+//  Created by Carlos Garcia-Muskat on 24/03/2019.
+//  Copyright © 2019 Carlos Garcia-Muskat. All rights reserved.
+//
+
+import Foundation
+class PetService {
+   
+}
